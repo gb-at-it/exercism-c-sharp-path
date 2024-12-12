f09f3d1b02948363678abfcc7bc6678732df1480
Giacomo Baldo <giacomo.baldo@moneycorp.com>
2024-12-12 17:18:19 +0000

    classes

---
+++ Classes/ElonsToy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class RemoteControlCar
+{
+    private int _distanceDriven = 0;
+    private int _batteryLevel = 100;
+
+    public static RemoteControlCar Buy()
+        => new();
+    public string DistanceDisplay()
+        => $"Driven {_distanceDriven} meters";
+    public string BatteryDisplay()
+        => _batteryLevel == 0 ? "Battery empty" : $"Battery at {_batteryLevel}%";
+    public void Drive()
+    {
+        if (_batteryLevel > 0)
+        {
+            _distanceDriven += 20;
+            _batteryLevel -= 1;
+        }
+    }
+}
