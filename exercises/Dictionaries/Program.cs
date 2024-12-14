4f202016185793db1dcb3d13c5eb91034c59c14d
Giacomo Baldo <giacomo.baldo@moneycorp.com>
2024-12-14 14:41:50 +0000

    dictionaries

---
+++ exercises/Dictionaries/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+var prePopulated = DialingCodes.GetExistingDictionary();
+Console.WriteLine(prePopulated.Count);
+
+var prePopulated2 = DialingCodes.GetExistingDictionary();
+Console.WriteLine(prePopulated2[91]);
+
+var countryCodes = DialingCodes.AddCountryToExistingDictionary(DialingCodes.GetExistingDictionary(), 44, "United Kingdom");
+Console.WriteLine(countryCodes.Count);
