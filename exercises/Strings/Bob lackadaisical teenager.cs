deb176f3faf80db8d951e35e82a57535e63011e9
Giacomo Baldo <giacomo.baldo@moneycorp.com>
2024-12-18 17:58:37 +0000

    strings and chars manipulation

---
+++ exercises/Strings/Bob lackadaisical teenager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Bob
+{
+    public static string Response(string statement)
+    {
+        statement = string.Concat(statement.Where(c => !char.IsWhiteSpace(c)));
+
+        if (string.IsNullOrEmpty(statement))
+            return "Fine. Be that way!";
+
+        var (isQuestion, isCapitalized) = (statement.EndsWith('?'), statement.Any(char.IsLetter) && statement.Where(char.IsLetter).All(char.IsUpper));
+
+        return (isQuestion, isCapitalized) switch
+        {
+            (true, true) => "Calm down, I know what I'm doing!",
+            (true, _) => "Sure.",
+            (_, true) => "Whoa, chill out!",
+            _ => "Whatever."
+        };
+    }
+}
