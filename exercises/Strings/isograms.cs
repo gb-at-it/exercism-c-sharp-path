816e60571e9ba4573ee1e26f9a2160cedc19e0ac
Giacomo Baldo <giacomo.baldo@moneycorp.com>
2024-12-19 13:51:47 +0000

    string and chars basic manipulation

---
+++ exercises/Strings/isograms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var letters = word.Where(char.IsLetter).Select(char.ToLower);
+        
+        return letters.Count() == letters.Distinct().Count();
+    }
+}
