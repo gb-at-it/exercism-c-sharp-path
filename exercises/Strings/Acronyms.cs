7b10d8195336f18e187cd4f039e02f2db05fe3aa
Giacomo Baldo <giacomo.baldo@moneycorp.com>
2024-12-19 15:35:19 +0000

    string manipulation with a little regex

---
+++ exercises/Strings/Acronyms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var helperPhrase = string.Concat(phrase.ToUpper().Replace('-', ' ').Where(c => char.IsLetter(c) || char.IsWhiteSpace(c)));
+        return string.Concat(helperPhrase.Split().Select(s => s.FirstOrDefault()).Where(char.IsLetter));          
+    }
+
+    public static string Abbreviate2(string phrase)
+    {
+        var helperPhrase = Regex.Replace(phrase.ToUpper().Replace('-', ' '), @"[^A-Z\s]", string.Empty);
+        return string.Concat(helperPhrase.Split().Select(s => s.FirstOrDefault()).Where(char.IsLetter));
+    }
+}
--- exercises/Strings/Program.cs
+++ exercises/Strings/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
+﻿//Console.WriteLine(Identifier.Clean("MyΟβιεγτFinder"));
 
-RotationalCipher.Rotate("omg", 5);
+//RotationalCipher.Rotate("omg", 5);
+Console.WriteLine(Acronym.Abbreviate("The Road _Not_ Taken"));
+Console.WriteLine(Acronym.Abbreviate2("The Road _Not_ Taken"));
 
